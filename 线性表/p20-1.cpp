42e705f7aab3c274566200d5480f411402fc802b
YangUper <1792543295@qq.com>
2026-03-02 20:10:19 +0800

    修改一处不规范

--- 线性表/p20-1.cpp
+++ 线性表/p20-1.cpp
@@ -1,5 +1,5 @@
 /*
- *P20-1
+ * P20-1
  * 题目：从顺序表中删除具有最小值的元素（假设唯一）并由函数返回被删元素的值。空出的位置由最后一个元素填补，若顺序表为空，则显示出错误信息并退出运行。
  */
 
@@ -19,7 +19,7 @@ bool initList(SqList& list){
     try {
         list.dataList = new int [10];
         list.length = 10;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < list.length; i++)
             list.dataList[i] = i;
         return true;
     }catch (const bad_alloc& e) {
